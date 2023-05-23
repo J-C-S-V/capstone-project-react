357fb709e98d1d77f5cdd4821191baebf3ce9cef
Juan Sanchez <juansan1993@hotmail.com>
2023-05-23 10:20:54 -0500

    Add api and display content on UI

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,49 @@
 import '../styles/App.scss';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchCards } from '../redux/features/cardSlice';
 
 function Card() {
-  const { card } = useSelector((state) => state.card);
+  const { card } = useSelector((store) => store.card);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCards());
+  }, [dispatch]);
+
   return (
     <div>
-      <h2>
-        {card}
-      </h2>
+      {card.map((item) => (
+        <article key={item}>
+          <header>
+            {item.title_english}
+          </header>
+          <img height={400} width={300} alt="test" src={item.images.webp.image_url} />
+          <p>
+            Duration:
+            {' '}
+            {item.duration}
+          </p>
+          <p>
+            Type:
+            {' '}
+            {item.type}
+          </p>
+          <p>
+            Episodes:
+            {' '}
+            {item.episodes}
+          </p>
+          <p>
+            Score:
+            {' '}
+            {item.score}
+          </p>
+          {/* // <h2 alt="test" key={item} src={item.mal_id}>{item.url}</h2> */}
+        </article>
+      ))}
     </div>
+    // <div>{card}</div>
   );
 }
 
--- src/redux/features/cardSlice.js
+++ src/redux/features/cardSlice.js
@@ -1,13 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const url = 'https://api.jikan.moe/v4/top/anime?filter=bypopularity';
+
+export const fetchCards = createAsyncThunk(
+  'cards/fetchCards',
+  async () => {
+    const response = await fetch(`${url}`);
+    const data = await response.json();
+    // Esto lo que retorna es un array, por eso hay
+    // que usar en el map item.name o item.id u otra cosa pueh
+    console.log('data', data.data);
+    return data.data;
+  },
+);
 
 const initialState = {
-  card: 'Juank',
+  card: [],
 };
 
 const cardsSlice = createSlice({
-  name: 'cards', // Esto no sirve de mucho
+  name: 'cards',
   initialState,
   reducers: {},
+  extraReducers: {
+    [fetchCards.fulfilled]: (state, action) => {
+      state.card = action.payload;
+    },
+  },
 });
 
 export default cardsSlice.reducer;
